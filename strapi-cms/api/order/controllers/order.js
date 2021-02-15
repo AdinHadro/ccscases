5115e54f039ed0ef92279ea53556347c88265629
DESKTOP-7U55V9L\adinh <adin.hadrovic@gmail.com>
2021-02-15 16:01:11 +0100

    Final changes with stripe integration installed

---
+++ strapi-cms/api/order/controllers/order.js
@@ -0,0 +1,49 @@
+"use strict";
+const stripe = require("stripe")("sk_test_51IIti8D4sdWxJpjKuwFJUvIMOAeGpzCTXsG5hAUxUEJUI7Dm7KFEMSHvDZNSR8qlvquL3mIiyaRhB9LqjjW4KnLO00fDV4q9Ls");
+
+module.exports = {
+  create: async (ctx) => {
+    const {
+      address,
+      price,
+      products,
+      postalCode,
+      token,
+      city,
+      buyerName,
+      buyerLastname,
+      buyerEmail
+    } = ctx.request.body;
+    console.log(ctx.request.body)
+    try {
+      await stripe.charges.create({
+        amount: products.map(x => x.price).reduce((a, b) => a + b, 0) * 100,
+        currency: "usd",
+        description: `Order created ${new Date()}`,
+        source: token,
+      });
+
+      try {
+        const order = await strapi.services.order.create({
+        //   user: ctx.state.user.id,
+
+          address,
+          amount: price,
+          products,
+          postalCode,
+          city,
+          buyerName,
+          buyerLastname,
+          buyerEmail,
+        });
+
+        return order;
+      } catch (err) {
+        console.log(err);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  
+};
