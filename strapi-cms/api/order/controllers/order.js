7c1c71be19e658fd338e70faf063254cd2a80eea
DESKTOP-7U55V9L\adinh <adin.hadrovic@gmail.com>
2021-03-01 22:37:07 +0100

    fixes on backend payment functinos fix and more

--- strapi-cms/api/order/controllers/order.js
+++ strapi-cms/api/order/controllers/order.js
@@ -1,51 +1,84 @@
 "use strict";
-const stripe = require("stripe")("sk_test_51IIti8D4sdWxJpjKuwFJUvIMOAeGpzCTXsG5hAUxUEJUI7Dm7KFEMSHvDZNSR8qlvquL3mIiyaRhB9LqjjW4KnLO00fDV4q9Ls");
+const stripe = require("stripe")(
+  "sk_test_51IIti8D4sdWxJpjKuwFJUvIMOAeGpzCTXsG5hAUxUEJUI7Dm7KFEMSHvDZNSR8qlvquL3mIiyaRhB9LqjjW4KnLO00fDV4q9Ls"
+);
+const axios = require("axios");
+
+const sendToAppCssCases = async (order) => {
+  await axios.post(
+    "https://app.ccscases.com/onboarding3.webapi",
+    {
+      order_date: new Date(),
+      order_number: order.id,
+      product_id: order.products[0].id,
+      order_quantity: 1,
+      customer_taxes: null,
+      quantity: 1,
+      price: order.amount,
+      first_name: order.buyerName,
+      last_name: order.buyerLastname,
+      address_1: order.address,
+      address_2: order.address2,
+      city: order.city,
+      state: order.state,
+      postcode: order.postalCode,
+      country: order.buyerCountry,
+      email: order.buyerEmail,
+      phone: order.buyerPhone,
+    },
+    {
+      headers: {
+        "webhook-secret": "Mtx6A8nkaBKoU4EWvvbhWxsHnoSCt",
+      },
+    }
+  );
+};
+
+const sendToStripe = async (body) => {
+  console.log(body)
+  await stripe.charges.create({
+    amount: body.products.map((x) => x.price).reduce((a, b) => a + b, 0) * 100,
+    currency: "usd",
+    description: `Order created ${new Date()}`,
+    source: body.token,
+  });
+};
+
+const saveInStrapi = async (body) => {
+  const order = await strapi.services.order.create(
+    {
+      //   user: ctx.state.user.id,
+
+      address: body.address,
+      address2: body.address2,
+      amount: body.price,
+      products: body.products,
+      postalCode: body.postalCode,
+      city: body.city,
+      state:body.state,
+      buyerCountry: body.buyerCountry,
+      buyerName: body.buyerName,
+      buyerLastname: body.buyerLastname,
+      buyerEmail: body.buyerEmail,
+      buyerPhone: body.buyerPhone,
+    }
+  );
+
+  return order;
+};
 
 module.exports = {
   create: async (ctx) => {
-    const {
-      address,
-      price,
-      products,
-      postalCode,
-      token,
-      city,
-      state,
-      buyerName,
-      buyerLastname,
-      buyerEmail
-    } = ctx.request.body;
-    console.log(ctx.request.body)
     try {
-      await stripe.charges.create({
-        amount: products.map(x => x.price).reduce((a, b) => a + b, 0) * 100,
-        currency: "usd",
-        description: `Order created ${new Date()}`,
-        source: token,
-      });
+      await sendToStripe(ctx.request.body);
 
-      try {
-        const order = await strapi.services.order.create({
-        //   user: ctx.state.user.id,
+      const order = await saveInStrapi(ctx.request.body);
 
-          address,
-          amount: price,
-          products,
-          postalCode,
-          city,
-          state,
-          buyerName,
-          buyerLastname,
-          buyerEmail,
-        });
+      await sendToAppCssCases(order);
 
-        return order;
-      } catch (err) {
-        console.log(err);
-      }
-    } catch (err) {
-      console.log(err);
+      return order;
+    } catch (error) {
+      console.error(error);
     }
   },
-  
 };
