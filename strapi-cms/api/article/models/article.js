9366fcda2314cb0ec8fcd44f58f7c9213bc9a791
DESKTOP-7U55V9L\adinh <adin.hadrovic@gmail.com>
2021-04-06 13:13:57 +0200

    Fixes of slugify

--- strapi-cms/api/article/models/article.js
+++ strapi-cms/api/article/models/article.js
@@ -11,11 +11,11 @@ module.exports = {
   lifecycles: {
     async beforeCreate(data) {
       strapi.log.debug("beforeCreate")
-      data.slug = slugify(data.title, {lower: true});
+      data.slug = slugify(data.title, {remove: /[*+~.()'"!?:@]/g, lower: true} );
     },
     async beforeUpdate (params,data) {
       strapi.log.debug("beforeUpdate")
-      data.slug = slugify(data.title, {lower: true});
+      data.slug = slugify(data.title,{remove: /[*+~.()'"!?:@]/g, lower: true} );
     },
   }
 };
